f5176324a9dca2fc6740d278650b110539f89266
hakimm77 <hakimboum406@gmail.com>
2022-11-11 23:00:35 +0100

    uploading files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Flex, Text } from "@chakra-ui/react";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Flex>
+        <Text>heybe</Text>
+      </Flex>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
